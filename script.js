27b0e7dedc051c852ddd26993d44fde4085c29bb
Thierry Murhondezi Bisimwa <thierrymurhondezi@gmail.com>
2023-08-02 04:31:08 +0200

    feat:add I declare two variables , userName and userAge , that will be filled with the information gotten from the user through the 'prompt' command in javascript

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 alert( "Bien venu dans notre programme Qui Booste votre Motivatoin journaliere au Zenith !! ");
+let userName= prompt("Quel est votre prenom");
+userName;
+let userAge= prompt("quel age avez vous ?");
+userAge;
